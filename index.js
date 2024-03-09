89a0347609a1d54c0a3e25d9e444b9ab027d56c3
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-03-09 21:01:25 +0530

    using chalk lib and gitIgnore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const app = require('./app.js')
 // console.log(app);
-const http = require("http");
+// const http = require("http");
 // http.createServer((req,resp)=>{
 //     resp.write('hello this is saurabh');
 //     resp.end();
@@ -9,8 +9,11 @@ const http = require("http");
 //   resp.write("Hello this is first program for using https server");
 //   resp.end();
 // }
-const dataControl = (req, resp) => {
-  resp.write("Hello this is first program for using https server");
-  resp.end();
-};
-http.createServer(dataControl).listen(3000);
+// const dataControl = (req, resp) => {
+//   resp.write("Hello this is first program for using https server");
+//   resp.end();
+// };
+// http.createServer(dataControl).listen(3000);
+let chalk =require('chalk')
+// console.log('hello',chalk.colorNames[red,green,yellow]);
+console.log(chalk.blue('Hello world!'));
