0f2840acfe4e8966d2f7ddcbe77b836c2c3ac0f1
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-04-10 16:58:05 +0530

    added connection and more methods

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const { MongoClient } = require("mongodb");
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
-async function getData() {
+// async function getData() {
+//   let result = await client.connect();
+//   let db = result.db("node-js");
+//   let collection = db.collection("node");
+//   let resp = await collection.find({name:'m40'}).toArray();
+//   console.log(resp );
+// }
+// getData();
+
+async function dbConnection() {
   let result = await client.connect();
   let db = result.db("node-js");
-  let collection = db.collection("node");
-  let resp = await collection.find({}).toArray();
-  console.log(resp);
+  return (collection = db.collection("node"));
 }
-getData();
+// dbConnection().then((res) => {
+//   res.find({name:'m40'}).toArray().then((data) => {
+//     console.log(data);
+//   });
+// });
+const main = async () => {
+  let data = await dbConnection();
+  data = await data.find().toArray();
+  console.warn(data);
+};
+
+main();
