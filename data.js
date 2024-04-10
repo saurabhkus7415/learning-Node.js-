91771ed96af0c1aad0acd2672818ceecd40fc617
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-04-10 12:54:45 +0530

    basic learnig completed

---
+++ data.js
@@ -0,0 +1,11 @@
+// const data = { name: "saurabh kushwaha", email: "Sanu@gmail.com" };
+const data ={data: [
+  { name: "saurabh kushwaha", email: "Sanu@gmail.com" },
+  { name: "saurabh kushwaha", email: "Sanu@gmail.com" },
+  { name: "saurabh kushwaha", email: "Sanu@gmail.com" },
+  { name: "saurabh kushwaha", email: "Sanu@gmail.com" },
+  { name: "saurabh kushwaha", email: "Sanu@gmail.com" },
+]};
+
+module.exports = data;
+ 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./app.js')
+// const app = require("./app.js");
 // console.log(app);
 // const http = require("http");
 // http.createServer((req,resp)=>{
@@ -14,6 +14,55 @@ const app = require('./app.js')
 //   resp.end();
 // };
 // http.createServer(dataControl).listen(3000);
-let chalk = require('chalk')
+// let chalk = require('chalk')
 // console.log('hello',chalk.colorNames[red,green,yellow]);
-console.log(chalk.blue('Hello world!'));
+// console.log(chalk.blue('Hello world!'));
+
+// CRETAE A NEW SERVICE WITH API CREATION
+const http = require("http");
+// http,
+//   http.createServer((req, resp) => {
+//     resp.writeHead(200, { "content-Type": "applicationjson" });
+//     resp.write(
+//       JSON.stringify({ name: "saurabh kushwaha", email: "Sanu@gmail.com" })
+//     );
+//     resp.end();
+//   }).listen(3000)
+// IMPORT DATA FROM OTHER JS FILE
+// const data = require("./data");
+// http
+//   .createServer((req, resp) => {
+//     resp.writeHead(200, { "content-Type": "applicationjson" });
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+//   })
+//   .listen(3000);
+
+// using express
+
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("", (req, resp) => {
+  console.log(req.query.name);
+  resp.send("Welcome," + req.query.name);
+});
+
+app.get("/about", (req, resp) => {
+  resp.send("Welcome, This is a About Page");
+});
+
+app.get("/help", (req, resp) => {
+  resp.send("Welcome, This is a Help Page");
+});
+
+app.get("/profile", (req, resp) => {
+  const user = {
+    name: "Amit",
+    mobile: "8787878785",
+  };
+  resp.render("profile", { user });
+});
+
+app.listen(5000);
