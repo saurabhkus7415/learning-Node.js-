94f160fcf20081d55634ec1bfcb0de525b6f6949
saurabhkus7415 <saurabhkus7415@gmail.com>
2024-04-10 16:42:15 +0530

    basic connection

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const express = require("express");
-const app = express();
-const reqFilter = require("./middleware");
-//  application  level middileware
-// app.use(reqFilter);
-const route = express.Router();
-route.use(reqFilter);
-app.get("/", (req, resp) => {
-  resp.send("Welcome to home page");
-});
-app.get("/users", (req, resp) => {
-  resp.send("Welcome to user page");
-});
-//  router level middileware
-app.get("/about", reqFilter, (req, resp) => {
-  resp.send("Welcome to about page");
-});
-
-// using export router for using middleware
-route.get("/contact", (req, resp) => {
-  resp.send("Welcome to contact page");
-});
-route.get("/help", (req, resp) => {
-  resp.send("Welcome to help page");
-});
-
-app.use("/", route);
-
-app.listen(5000);
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+async function getData() {
+  let result = await client.connect();
+  let db = result.db("node-js");
+  let collection = db.collection("node");
+  let resp = await collection.find({}).toArray();
+  console.log(resp);
+}
+getData();
